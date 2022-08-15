47295afdfaffe01dcb79f6b7f8791850205c84ad
pan9 <438063442@qq.com>
2022-08-15 23:05:01 +0800

    Change pins.

--- extensions/arduino/communication/infrareRecv/toolbox.js
+++ extensions/arduino/communication/infrareRecv/toolbox.js
@@ -5,10 +5,10 @@ function addToolbox () {
     return `
 <category name="%{BKY_DBITINFRARERECV}" id="DBITINFRARERECV" colour="#5867D6" secondaryColour="#5867D6">
     <block type="DBitInfrareRecv_read">
-        <field name="PIN">A0-A1</field>
+        <field name="PIN">D2-D3</field>
     </block>
     <block type="DBitInfrareRecv_detect">
-        <field name="PIN">A0-A1</field>
+        <field name="PIN">D2-D3</field>
         <field name="KEY">1</field>
     </block>
 </category>`;
